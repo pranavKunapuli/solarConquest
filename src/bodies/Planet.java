73a9173d8b9c9816178ef4c0747b4a298976685e
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2013-12-02 17:36:05 -0500

    Updating game functionality from enemy ships to poisonous planets

--- src/bodies/Planet.java
+++ src/bodies/Planet.java
@@ -10,7 +10,7 @@ public class Planet {
 	private int x; // X coordinate
 	private int y; // Y coordinate
 	private int forces; // Amount of ships around planet
-	private int enemy; // Amount of enemy forces around planet
+	private int enemy; // Rate of deterioration of user's ships
 	private Territory t; // Whether user has conquered the planet or not
 	private java.awt.Color planetColor; // Planet display color
 	
@@ -40,57 +40,6 @@ public class Planet {
 	public int getForces() { return this.forces; }
 	
 	public int getEnemy() { return this.enemy; }
-	
-	public int attack() {
-		forces = Math.round(forces / 2);
-		return forces;
-	}
-	
-	private void battle() {
-		if(enemy != 0 || forces != 0) {
-			forces -= 2;
-			enemy -= 2;
-			if (forces < 0) { forces = 0; }
-			if (enemy < 0) { enemy = 0; }
-		}
-	}
-	
-	/** Initially sorts out the conflict between user forces and enemy forces.
-	 *  Reduces the health of the planet if there are no remaining enemy
-	 *  forces. Health stays unchanged if enemy forces remain. Program then
-	 *  pauses for 1 second.
-	 */
-	public void deltaHealth() {
-		battle();
-		if (enemy == 0 || forces == 0) { health -= 2; }
-		if (health < 0) {
-			health = original;
-			switch(t) {
-			case USER: 
-				t = Territory.ENEMY;
-				break;
-			case ENEMY: 
-				t = Territory.USER;
-				break;
-			case NEUTRAL: 
-				if(enemy == 0) {
-					t = Territory.USER;
-				}
-				else if (forces == 0) {
-					t = Territory.ENEMY;
-				}
-				break;
-			}
-			setColor();
-		}
-		else {
-			try {
-			    Thread.sleep(1000);
-			} catch(InterruptedException ex) {
-			    Thread.currentThread().interrupt();
-			}
-		}
-	}
 	
 	public void draw (Graphics g) {
 		g.setColor(planetColor);
